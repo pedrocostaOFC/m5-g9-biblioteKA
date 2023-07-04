af3f5c35e58ae3d373cfe7cd595c516a31426e7c
Leonardo Cunha Balhana <leo.cb@hotmail.co.uk>
2023-07-04 15:49:56 -0300

    feat: model and serializer without relations

---
+++ copies/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Copy
+
+class CopySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Copy
+        fields = [
+            'id',
+            'is_avaiable'
+        ]
