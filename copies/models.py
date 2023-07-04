03daa9def8cc1f941c4816de7fa7ea8f8439fe57
Leonardo Cunha Balhana <leo.cb@hotmail.co.uk>
2023-07-04 17:32:42 -0300

    feat: CopyView, URL

--- copies/models.py
+++ copies/models.py
@@ -5,3 +5,9 @@ class Copy(models.Model):
         ordering = ['id']
     
     is_avaiable = models.BooleanField(default=True)
+
+    book = models.ForeignKey(
+        'books.Book',
+        on_delete=models.CASCADE,
+        related_name='copy',
+    )
