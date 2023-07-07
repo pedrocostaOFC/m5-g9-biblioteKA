c930b94099e7f9c39082dfc62f420aaa52f053b2
leonardo-cb <leo.cb@hotmail.co.uk>
2023-07-07 09:12:39 -0300

    feat: fix create, develop permissions and urls

---
+++ copies/permissions.py
@@ -0,0 +1,5 @@
+from rest_framework import permissions
+
+class IsCollaborator(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.method in permissions.SAFE_METHODS or request.user.is_authenticated and request.user.is_superuser
