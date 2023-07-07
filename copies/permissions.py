d76ead3f12d99a03535ef3c8910b55596e7314a5
Renan Felipe Cruz <58148864+Renanfcsb@users.noreply.github.com>
2023-07-07 13:28:22 -0400

    Revert "Feat/copies"

--- copies/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import permissions
-
-class IsCollaborator(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS or request.user.is_authenticated and request.user.is_superuser
--- copies/serializers.py
+++ copies/serializers.py
@@ -1,15 +1,11 @@
 from rest_framework import serializers
 from .models import Copy
-from books.serializers import BookSerializer
 
 class CopySerializer(serializers.ModelSerializer):
-
-    book = BookSerializer(read_only=True)
-    
     class Meta:
         model = Copy
         fields = [
             'id',
-            'is_avaiable',
-            'book'
+            'is_avaiable'
         ]
+        read_only_fields = ['book']
