4687f647e3665fd45f35d0f3246d5cb64b6ad6d9
leonardo-cb <leo.cb@hotmail.co.uk>
2023-07-10 16:12:44 -0300

    fix: retrieve and delete copy

--- copies/urls.py
+++ copies/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("copies/", views.CopyView.as_view()),
-    path("copies/<int:copy_id>/", views.CopyDetailView.as_view()),
+    path("copies/<int:pk>/", views.CopyDetailView.as_view()),
 ]
