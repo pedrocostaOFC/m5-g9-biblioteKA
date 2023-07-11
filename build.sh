c8aa8b26d9b6dbcaf4ae916e56d4fce24977287a
Renan.dev <dev.rfcsb@gmail.com>
2023-07-11 14:18:50 -0400

    fix super user

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py syncdb --noinput
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
 python manage.py migrate
