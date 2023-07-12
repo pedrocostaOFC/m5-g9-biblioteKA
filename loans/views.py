576bad190bfbd1de85de817a0539122b9d845552
arthwrjf <artur.infected@gmail.com>
2023-07-11 21:28:07 -0300

    loan date and blocked user check

--- loans/views.py
+++ loans/views.py
@@ -1,32 +1,38 @@
 from rest_framework import generics
-from rest_framework.views import Request, Response, status
+from rest_framework.views import status
 from django.shortcuts import get_object_or_404, render
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from datetime import timedelta, date
-from django.utils import timezone
+from datetime import date
 from loans.models import Loan
 from users.models import User
 from copies.models import Copy
 from books.models import Book
 from .permissions import IsCollaborator
-from loans.serializers import ListLoanSerializer, CreateLoanSerializer, CreateReturnSerializer
+from loans.serializers import CreateLoanSerializer
 from rest_framework.exceptions import APIException
 
 class ListCreateLoanView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsCollaborator]
-
     queryset = Loan.objects.all()
     serializer_class = CreateLoanSerializer
 
     def perform_create(self, serializer):
-        copy = get_object_or_404(Copy, pk=self.request.data["copy_id"])
-        user = get_object_or_404(User, pk=self.request.data["user_id"])
-
+        copy_id = self.request.data.get("copy_id")
+        user_id = self.request.data.get("user_id")
+        if not copy_id or not user_id:
+            raise APIException("Copy ID and User ID must be provided.", code=status.HTTP_400_BAD_REQUEST)
+        copy = get_object_or_404(Copy, pk=copy_id)
+        user = get_object_or_404(User, pk=user_id)
+        book = get_object_or_404(Book, pk= copy.book.id)
         if user.is_blocked:
             raise APIException("User is blocked.", code=status.HTTP_403_FORBIDDEN)
-
+        if book.avaiable_copies > 0:
+            book.avaiable_copies -= 1
+            book.save()
+        else:
+            raise APIException("No more copies available.", code=status.HTTP_400_BAD_REQUEST)
         serializer.save(user=user, copy=copy)
 
 
