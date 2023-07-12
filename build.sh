33c66fc728030a81c4874efa6fe6869ef811b6f2
Renan Felipe Cruz <58148864+Renanfcsb@users.noreply.github.com>
2023-07-12 11:38:18 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
 python manage.py migrate
